2e99e630891058d05af1eff1158ce9a0ed902cbd
MHLinh <56354185+MHLinh@users.noreply.github.com>
2019-10-14 20:34:31 +0100

    Update Question1.py

--- python/Question1.py
+++ python/Question1.py
@@ -1,5 +1,26 @@
 # modify this function, and create other functions below as you wish
 def question01(initialLevelOfDebt, interestPercentage, repaymentPercentage):
-    # modify and then return the variable below
-    answer = -5
+    initialLevelOfDebt = int(input())
+    interestPercentage = int(input())
+    repaymentPercentage = int(input())
+
+    totalPayment = int(0)
+    currentDebt = int(initialLevelOfDebt)
+    deposit = int(initialLevelOfDebt*0.1)
+    repayment = int(initialLevelOfDebt*repaymentPercentage/100)
+
+    if repayment < 50:
+        repayment = 50
+
+    while currentDebt > 0:
+        currentDebt = currentDebt + currentDebt*interestPercentage/100
+        if currentDebt < repayment:
+            totalPayment += int(currentDebt)
+            currentDebt = 0
+            break
+        else:
+            currentDebt -= int(repayment)
+            totalPayment += int(repayment)
+    totalPayment += deposit
+    answer = int(totalPayment)
     return answer
